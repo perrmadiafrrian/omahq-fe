94d671e70c0147e7c310da5babc1838b9537a853
R <rian@sandbox.io>
2021-07-06 15:04:45 +0700

    page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,9 @@
 const Login = (props) => {
   return (
     <section className="text-gray-700 bg-gray-100">
-      <div className="container xl:px-32 px-5 py-36 mx-auto flex flex-wrap items-center">
-        <div className="w-full pr-0">
-          <h1 className="title-font font-bold lg:text-7xl text-6xl text-blue-600 text-center">
+      <div className="container 2xl:px-100 lg:px-80 px-5 py-36 mx-auto flex flex-wrap items-center">
+        <div className="w-full pr-0 xl:mb-4 lg:mb-3 md:mb-2">
+          <h1 className="title-font font-bold lg:text-7xl text-6xl text-yellow-600 text-center">
             OmahQ
           </h1>
           <p className="leading-relaxed mt-4 lg:text-3xl text-2xl font-medium text-black text-center">
@@ -27,10 +27,10 @@ const Login = (props) => {
               className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-lg outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
           </div>
-          <button className="text-white border-0 py-2 px-8 focus:outline-none font-medium rounded text-xl bg-blue-600">
+          <button className="text-white border-0 py-2 px-8 focus:outline-none font-medium rounded text-xl bg-yellow-600">
             Log In
           </button>
-          <p className="text-sm text-blue-500 py-5 text-center">
+          <p className="text-sm text-yellow-500 py-5 text-center">
             Forgot Password?
           </p>
         </div>
