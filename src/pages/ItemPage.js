bef98da71c4e9980d2a9a4bff04271b4560dc7f7
R <rian@sandbox.io>
2021-07-16 16:45:18 +0700

    item

--- src/pages/ItemPage.js
+++ src/pages/ItemPage.js
@@ -8,13 +8,13 @@ const ItemPage = (props) => {
         <div className="pt-6 pb-4 mb-4 bg-indigo-50 px-6 w-full flex justify-between shadow">
           <div className="px-6 md:px-16 text-3xl font-bold">House 1 -</div>
           <div className="px-6 md:px-16">
-            <button className="bg-green-500 hover:bg-green-600 rounded-lg py-2 px-4 text-white">
+            <button className="bg-green-500 hover:bg-green-600 duration-300 ease-in-out rounded-lg py-2 px-4 text-white">
               Scan
             </button>
           </div>
         </div>
         <div className="w-full flex flex-wrap justify-center space-x-6">
-          <div className="w-72 h-44 bg-blue-200 hover:bg-blue-300 duration-300 ease-in-out shadow rounded-2xl"></div>
+          <div className="w-80 h-44 bg-white hover:bg-yellow-50 duration-300 ease-in-out shadow-md rounded-2xl"></div>
         </div>
       </div>
     </div>
