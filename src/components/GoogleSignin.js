e1c3b2c741ac3ae3c04ed6749ddb0d0e578a2fa4
R <rian@sandbox.io>
2021-07-23 08:22:46 +0700

    adding axios

--- src/components/GoogleSignin.js
+++ src/components/GoogleSignin.js
@@ -1,13 +1,25 @@
 import { useGoogleLogin } from "react-google-login";
 import googleIcon from "../icons/google.svg";
-import refreshTokenSetup from "../utils/refreshTokenHandler";
+import axiosInstance from "../utils/axiosInstance";
+// import refreshTokenSetup from "../utils/refreshTokenHandler";
 
 const CLIENT_ID = `2615891792-9iab23mglp6sch2dn81p3ugmfvjgfakc.apps.googleusercontent.com`;
 
 function GoogleSignin() {
-  const onSuccess = (res) => {
-    console.log(res);
-    refreshTokenSetup(res);
+  console.log(axiosInstance);
+
+  const onSuccess = async (res) => {
+    console.log(res.profileObj);
+    await axiosInstance
+      .post("/auth/google", {
+        id_token: "abc",
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        // alert(err.response.data);
+      });
   };
 
   const onFailure = (res) => {
@@ -25,7 +37,7 @@ function GoogleSignin() {
   return (
     <button
       onClick={signIn}
-      className="w-full text-blue-600 bg-white border-0 py-2 px-8 shadow-lg focus:outline-none font-medium rounded text-xl relative"
+      className="w-full text-blue-600 bg-white border-0 py-2 px-8 shadow-lg hover:bg-gray-300 active:bg-gray-100 focus:outline-none font-medium rounded text-xl relative"
     >
       <img src={googleIcon} alt="Google Icon" className="w-8 left-4 absolute" />
       <span>Sign In</span>
