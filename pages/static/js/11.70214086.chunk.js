772003c355cf43ff32f29edf0a96cb533aef707d
perrmadiafrrian <permadi.afrian@gmail.com>
2021-08-09 01:36:57 +0000

    Deploying to gh-page from @ perrmadiafrrian/omahq-fe@9901aa3627098b9df57b76d9a738abe74c2fd516 🚀

---
+++ pages/static/js/11.70214086.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpomahq-fe"]=this["webpackJsonpomahq-fe"]||[]).push([[11],{191:function(t,a,h){"use strict";h.d(a,"a",(function(){return e}));var n=h(0);function e(t){return Object(n.a)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M256 19.27L25.637 249.638 19.27 256 32 268.73l6.363-6.367L256 44.727l217.637 217.636L480 268.73 492.73 256l-6.367-6.363zM96 48v107.273l64-64.002V48zm160 20.727l-192 192V486h64V320h96v166h224V260.727zM288 320h96v80h-96z"}}]})(t)}}}]);
+//# sourceMappingURL=11.70214086.chunk.js.map
